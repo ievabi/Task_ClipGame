9070d907ec6301ed0be3e2d9f869fda0d5e2e63c
ievabi <ibidermane@gmail.com>
2021-04-08 19:20:28 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,60 +1,54 @@
+let totalCoins = 0;
+let toSpendCoins = 0;
+let smallUpgradeCount = 0;
+let smallUpgradePrice = 10;
+let bigUpgradeCount = 0;
+let bigUpgradePrice = 90;
 
-//takes account for the coins per sec
 setInterval(function () {
-    let coinsPerSec1 = parseInt(document.querySelector('#upgrade-1').innerHTML);
-    let coinsPerSec10 = parseInt(document.querySelector('#upgrade-10').innerHTML);
-    let coinsperSec = coinsPerSec1 + 10 * coinsPerSec10;
-    document.querySelector('#total-coins').innerHTML = parseInt(document.querySelector('#total-coins').innerHTML) + coinsperSec;
-    document.querySelector('#spend-coins').innerHTML = parseInt(document.querySelector('#spend-coins').innerHTML) + coinsperSec;
-}, 1000);
-
-//check for button disable/enable
-setInterval(function () {
-    let availableCoins = parseInt(document.querySelector('#spend-coins').innerHTML);
-    let upgradePrice1 = parseInt(document.querySelector('#upgrade-1-price').innerHTML);
-    let upgradePrice10 = parseInt(document.querySelector('#upgrade-10-price').innerHTML);
-    if (availableCoins >= upgradePrice1) {
+    if (toSpendCoins >= smallUpgradePrice) {
         document.querySelector('#upgrade-1-btn').disabled = false;
     } else {
         document.querySelector('#upgrade-1-btn').disabled = true;
-    }
-    if (availableCoins >= upgradePrice10) {
+    };
+
+    if (toSpendCoins >= bigUpgradePrice) {
         document.querySelector('#upgrade-10-btn').disabled = false;
     } else {
         document.querySelector('#upgrade-10-btn').disabled = true;
-    }
-}, 50);
+
+    };
+
+    totalCoins += (smallUpgradeCount + (10 * bigUpgradeCount)) / 100;
+    toSpendCoins += (smallUpgradeCount + (10 * bigUpgradeCount)) / 100;
+    document.querySelector('#total-coins').innerHTML = Math.floor(totalCoins);
+    document.querySelector('#spend-coins').innerHTML = Math.floor(toSpendCoins);
+    document.querySelector('#upgrade-1').innerHTML = smallUpgradeCount;
+    document.querySelector('#upgrade-10').innerHTML = bigUpgradeCount;
+    document.querySelector('#upgrade-1-price').innerHTML = parseFloat(smallUpgradePrice.toFixed(1));
+    document.querySelector('#upgrade-10-price').innerHTML = parseFloat(bigUpgradePrice.toFixed(1));
+}, 10);
+
 
 //click button listener
 document.querySelector('#click-me').addEventListener('click', function () {
-    let availableCoins = parseInt(document.querySelector('#spend-coins').innerHTML);
-    let totalCoins = parseInt(document.querySelector('#total-coins').innerHTML);
-    availableCoins += 1;
-    totalCoins += 1;
-    document.querySelector('#spend-coins').innerHTML = availableCoins;
-    document.querySelector('#total-coins').innerHTML = totalCoins;
+    totalCoins++;
+    toSpendCoins++;
 });
 
 //1x upgrade button listener
 document.querySelector('#upgrade-1-btn').addEventListener('click', function () {
-    let availableCoins = parseInt(document.querySelector('#spend-coins').innerHTML);
-    let upgrades = parseInt(document.querySelector('#upgrade-1').innerHTML);
-    let upgradePrice1 = parseInt(document.querySelector('#upgrade-1-price').innerHTML);
-    document.querySelector('#spend-coins').innerHTML = availableCoins - upgradePrice1;
-    upgradePrice1 *= 1.1;
-    document.querySelector('#upgrade-1-price').innerHTML = Math.floor(upgradePrice1);
-    document.querySelector('#upgrade-1').innerHTML = upgrades + 1;
+    smallUpgradeCount++;
+    toSpendCoins -= smallUpgradePrice;
+    smallUpgradePrice *= 1.1;
+
 })
 
 //10x upgrade button listener
 document.querySelector('#upgrade-10-btn').addEventListener('click', function () {
-    let availableCoins = parseInt(document.querySelector('#spend-coins').innerHTML);
-    let upgrades = parseInt(document.querySelector('#upgrade-10').innerHTML);
-    let upgradePrice10 = parseInt(document.querySelector('#upgrade-10-price').innerHTML);
-    document.querySelector('#spend-coins').innerHTML = availableCoins - upgradePrice10;
-    upgradePrice10 *= 1.1;
-    document.querySelector('#upgrade-10-price').innerHTML = Math.floor(upgradePrice10);
-    document.querySelector('#upgrade-10').innerHTML = upgrades + 1;
+    bigUpgradeCount++;
+    toSpendCoins -= bigUpgradePrice;
+    bigUpgradePrice *= 1.1;
 })
 
 
